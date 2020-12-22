a0c5815af8da5e7224385b11f93475a120c80f90
Josh Frank <eurosewusa@gmail.com>
2020-12-22 09:31:29 -0500

    praline

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize( this_url )
+        @url = this_url
+    end
+
+    def get_response_body
+        uri = URI.parse( self.url )
+        response = Net::HTTP.get_response( uri )
+        response.body
+    end
+
+    def parse_json
+        JSON.parse( self.get_response_body )
+    end
+
+end
